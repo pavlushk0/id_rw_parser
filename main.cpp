9e23aabe24c9465d3c19008ebf629e97b96ca890
pavlushk0 <pavlushk0@mail.ru>
2018-11-26 22:34:46 +0400

    DT

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 
 #include <iostream>
+#include <fstream>
 
-int main(int argc, char **argc) {
+using namespace std;
+
+void usage() {
+	cout << "Please, place usage here later" << endl;
+}
+
+int main(int argc, char **argv) {
+	ifstream pfile;
+
+	if ((argc < 2) || (argc > 3)) {
+		cout << "wrong count of args!" << endl;
+		usage();
+		return 0;
+	}
+	
+	if (std::string(argv[1]) == "-h") {
+		usage();
+		return 0;
+	}
+	
+	if (std::string(argv[1]) == "-f") {
+		pfile.open(argv[2]);
+
+		if (pfile.is_open()) {
+
+		} else {
+			cout << "no file " << argv[2] << endl;
+			return 0;
+		}
+
+		pfile.close();
+	}
+
+	
 	return 0;
 }
